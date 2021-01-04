20e8bea2a70d3ebe8134a04eb8fe919eb1d48403
Jeremy Mann <jmann277@gmail.com>
2021-01-04 06:55:56 -0800

    make fzf better, e.g. function to open in editor

--- .config/fzf/fzf.zsh
+++ .config/fzf/fzf.zsh
@@ -11,3 +11,8 @@ fi
 # Key bindings
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# Make show hidden files
+
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
