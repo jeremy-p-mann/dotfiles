317905607fdb70a8522d07ab47bdeb68d8ed48b6
Jeremy Mann <jmann277@gmail.com>
2021-01-10 20:40:13 -0800

    made cd with fzf bindkey compatible

--- .config/fzf/fzf.zsh
+++ .config/fzf/fzf.zsh
@@ -16,3 +16,4 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
