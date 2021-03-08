0da9c7632f33d433f2829a59ce47710976772d7d
Jeremy Mann <jmann277@gmail.com>
2021-03-08 10:39:39 -0800

    move telescope settings to lua/jer folder

---
+++ .config/nvim/lua/jer/telescope.lua
@@ -0,0 +1,42 @@
+local actions = require('telescope.actions')
+require('telescope').setup{
+  defaults = {
+    initial_mode = "insert",
+    layout_strategy = "flex",
+    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+    mappings = {
+      i = {
+        ["<C-x>"] = false,
+        ["<C-s>"] = actions.goto_file_selection_split,
+        ["<C-c>"] = actions.close,
+      },
+    },
+    file_previewer = require'telescope.previewers'.vim_buffer_cat.new, 
+    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new, 
+  }
+}
+
+local M = {}
+M.search_dotfiles = function()
+    require("telescope.builtin").find_files({
+        prompt_title = "Dotfiles",
+        cwd = "$HOME/.config/"
+    })
+end
+
+current_directory = vim.fn.getcwd()
+tests_folder = current_directory .. "/tests/"
+
+function M.find_tests ()
+    require("telescope.builtin").find_files({
+        prompt_title = "Tests",
+        cwd = tests_folder
+    })
+end
+ 
+
+return M
+
+
