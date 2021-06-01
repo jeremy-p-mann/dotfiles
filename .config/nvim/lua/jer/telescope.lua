165e65d555421974957c5eb7097b8de69b4ee1f1
Jeremy Mann <jmann277@gmail.com>
2021-05-31 17:48:24 -0700

    find classes, fixture, copy to clipboard with telescope

--- .config/nvim/lua/jer/telescope.lua
+++ .config/nvim/lua/jer/telescope.lua
@@ -18,14 +18,35 @@ function M.find_tests ()
     })
 end
 
-
-
 function M.find_in_current_directory ()
     require("telescope.builtin").find_files({
-        prompt_title = "Tests",
+        prompt_title = "Files",
         cwd = vim.fn.expand('%'):match('.*/')
     })
 end
  
+function M.find_classes ()
+    require("telescope.builtin").grep_string({
+        prompt_title = "Classes",
+        search='class ',
+        use_regex=True
+    })
+end
+
+function M.find_fixtures ()
+    require("telescope.builtin").grep_string({
+        prompt_title = "Fixtures",
+        search='fixture',
+        use_regex=True
+    })
+end
+
+function M.copy_path_to_clipboard(prompt_bufnr, map)
+    local content = require('telescope.actions.state').get_selected_entry(prompt_bufnr)
+    vim.fn.system('echo ' .. content.value .. ' | pbcopy')
+    require('telescope.actions').close(prompt_bufnr)
+    return true
+end
 
 return M
+
