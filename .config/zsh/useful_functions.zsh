51a331eb0ac29f6e43cefbf1af61f5cbfca1d2d2
Jeremy Mann <jmann277@gmail.com>
2021-09-06 13:37:55 -0700

    add functions to open files with telescope

--- .config/zsh/useful_functions.zsh
+++ .config/zsh/useful_functions.zsh
@@ -1,7 +1,13 @@
 # fe [FUZZY PATTERN] - Open the selected file with the default editor
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
-e() {
+E() {
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+}
+et() {
+    nvim . -c ":lua require('jer.telescope').find_tests()"
+}
+e() {
+    [[ -n $1 ]] && nvim $1 || nvim . -c ":lua require('telescope.builtin').git_files()"
 }
