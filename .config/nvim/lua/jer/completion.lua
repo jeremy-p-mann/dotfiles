1da1714faacebc6a97630bc7c1892253629eb630
Jeremy Mann <jmann277@gmail.com>
2021-11-10 08:38:50 -0800

    refactor completion config

--- .config/nvim/lua/jer/completion.lua
+++ .config/nvim/lua/jer/completion.lua
@@ -1,10 +1,14 @@
--- Setup nvim-cmp.
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 local lspkind = require "lspkind"
 lspkind.init()
 local cmp = require'cmp'
 
 cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
     mapping = {
       ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
@@ -17,20 +21,26 @@ cmp.setup({
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources ={
+      { name = 'ultisnips' },
       { name = 'nvim_lsp' },
+      { name = 'nvim_lua' },
       { name = 'buffer' },
+      { name = 'path' },
     },
   formatting = {
     format = lspkind.cmp_format {
       with_text = true,
       menu = {
         buffer = "[buf]",
+        nvim_lua = "[api]",
         nvim_lsp = "[LSP]",
+        path = "[path]",
+        ultisnips= "[snip]",
       },
     },
   },
     experimental = {
-    native_menu = false,
+      native_menu = false,
 
   },
 })
