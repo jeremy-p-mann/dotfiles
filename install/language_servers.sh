86cec61addb0157e304eadce4b2396260b75ef1f
Jeremy Mann <jmann277@gmail.com>
2021-12-07 10:08:56 -0800

    make install lsp script

--- install/language_servers.sh
+++ install/language_servers.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+pip install python-lsp-server[all]
+npm install -g typescript typescript-language-server
+yarn global add yaml-language-server
 pip3 install --upgrade python-lsp-server \
     pylsp-mypy \
     pyls-flake8 \
