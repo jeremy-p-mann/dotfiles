609cf0bc25bddada620063fb37d7f748d30a112f
Jeremy Mann <jmann277@gmail.com>
2022-01-13 17:36:26 -0800

    add vim-surround

--- .config/nvim/lua/jer/plugins.lua
+++ .config/nvim/lua/jer/plugins.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
+  use 'tpope/vim-surround'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
