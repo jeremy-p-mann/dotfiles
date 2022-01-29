b4309b50a9a33820e7de13b750819d5b39615121
Jeremy Mann <jmann277@gmail.com>
2022-01-29 11:19:46 -0800

    make the lsp remaps actually work

--- .config/nvim/lua/jer/lsp.lua
+++ .config/nvim/lua/jer/lsp.lua
@@ -3,13 +3,11 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local on_attach=function()
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+      vim.keymap.set("n", "gd",   "<CMD>Telescope lsp_definitions<CR>", {buffer=0})
+      vim.keymap.set("n", "gi",  "<CMD>Telescope lsp_implementations<CR>", {buffer=0})
       vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-      vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0})
-      vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0})
-      vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer=0})
-      vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
+      vim.keymap.set("n", '<leader>rf', require'telescope.builtin'.lsp_references)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {buffer=0})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer=0})
     end
 
@@ -18,13 +16,13 @@ local opts = {
     on_attach=on_attach,
 }
 
-require'lspconfig'.pylsp.setup{opts}
+require'lspconfig'.pylsp.setup(opts)
 
-require'lspconfig'.tsserver.setup{opts}
+require'lspconfig'.tsserver.setup(opts)
 
-require'lspconfig'.bashls.setup{opts}
+require'lspconfig'.bashls.setup(opts)
 
-require'lspconfig'.rust_analyzer.setup{opts}
+require'lspconfig'.rust_analyzer.setup(opts)
 
 -- LUA
 -- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
