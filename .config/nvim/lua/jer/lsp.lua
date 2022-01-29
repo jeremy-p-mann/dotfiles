b5079164d66d93e4c02180dd6d3cb6ef0e796e29
Jeremy Mann <jmann277@gmail.com>
2022-01-29 09:33:11 -0800

    fix bug with lsp setup

--- .config/nvim/lua/jer/lsp.lua
+++ .config/nvim/lua/jer/lsp.lua
@@ -1,22 +1,30 @@
+--- LSP Related Stuff ---
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local on_attach=function()
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+      vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+      vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0})
+      vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0})
+      vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer=0})
+      vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer=0})
+    end
 
-
-require'lspconfig'.pylsp.setup{
-    capabilities=capabilities
+local opts = {
+    capabilities=capabilities,
+    on_attach=on_attach,
 }
 
-require'lspconfig'.tsserver.setup{
-    capabilities=capabilities
-}
+require'lspconfig'.pylsp.setup{opts}
 
-require'lspconfig'.bashls.setup{
-    capabilities=capabilities
-}
+require'lspconfig'.tsserver.setup{opts}
 
-require'lspconfig'.rust_analyzer.setup{
-    capabilities=capabilities
-}
+require'lspconfig'.bashls.setup{opts}
+
+require'lspconfig'.rust_analyzer.setup{opts}
 
 -- LUA
 -- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
@@ -41,6 +49,7 @@ table.insert(runtime_path, "lua/?/init.lua")
 
 require'lspconfig'.sumneko_lua.setup {
   capabilities=capabilities,
+  on_attach=on_attach,
   cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
   -- on_attach=require'completion'.on_attach,
   settings = {
