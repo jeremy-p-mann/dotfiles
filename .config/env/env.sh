0f9e8c8cecc2bea4b36c953784a02da6619d7f52
Jeremy Mann <jmann277@gmail.com>
2022-01-29 09:05:49 -0800

    update environment variables

--- .config/env/env.sh
+++ .config/env/env.sh
@@ -1,10 +1,19 @@
+if [ $(uname) = "Darwin" ]; then
+    export BASEDIR=$HOME/Documents
+else
+    export BASEDIR=$HOME
+fi
+export DOTFILEDIR=$BASEDIR/dotfiles
+export XDG_HOME_CONFIG=$DOTFILEDIR/.config
 export CLICOLOR=1
-export EDITOR="nvim"
-export IPYTHONDIR="~/.config/ipython"
-export TASKRC="~/.config/task/taskrc"
+export EDITOR=nvim
+export IPYTHONDIR=$XDG_HOME_CONFIG/ipython
+export TASKRC=$XDG_HOME_CONFIG/task/taskrc
 export LESSHISTFILE="-"
-export PTPYTHON_CONFIG_HOME="$HOME/.config/ptpython"
+export PTPYTHON_CONFIG_HOME="$XDG_HOME_CONFIG/ptpython"
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export ZDOTDIR=$HOME/.config/zsh
-export TMUX_CONFIG="$XDG_HOME_CONFIG/tmux/.tmux.conf"
+export ZDOTDIR=$XDG_HOME_CONFIG/zsh
+export ANSIBLE_CONFIG=$XDG_HOME_CONFIG/ansible/ansible.cfg
+export TMUX_CONFIG=$XDG_HOME_CONFIG/tmux/.tmux.conf
+
