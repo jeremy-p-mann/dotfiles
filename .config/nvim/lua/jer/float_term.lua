99a66ae4dbb9de46e9074bd725431d10b4d91050
Jeremy Mann <jmann277@gmail.com>
2022-02-07 08:26:34 -0800

    increase floating term size

--- .config/nvim/lua/jer/float_term.lua
+++ .config/nvim/lua/jer/float_term.lua
@@ -3,10 +3,10 @@ M.execute_code = function()
   local current_file = vim.fn.expand "%"
   local filetype = vim.bo.filetype
   if filetype == "python" then
-    local command = "FloatermNew! --height=0.9 --width=0.9 python3 " .. current_file
+    local command = "FloatermNew! --height=0.99 --width=0.99 python3 " .. current_file
     vim.cmd(command)
   elseif filetype == "lua" then
-    local command = "FloatermNew! --height=0.9 --width=0.9 lua " .. current_file
+    local command = "FloatermNew! --height=0.99 --width=0.99 lua " .. current_file
     vim.cmd(command)
   end
 end
