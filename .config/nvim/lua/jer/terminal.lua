b95a815436e05d6456cae88e61ed5983991829fc
Jeremy Mann <jmann277@gmail.com>
2022-03-13 10:23:44 -0700

    refactor execute code to terminal module

--- .config/nvim/lua/jer/terminal.lua
+++ .config/nvim/lua/jer/terminal.lua
@@ -1,8 +1,19 @@
 local nremap = require("jer.keymaps").nremap
 vim.g.floaterm_keymap_toggle = "<C-f>"
 
+local execute_code = function()
+  local current_file = vim.fn.expand "%"
+  local filetype = vim.bo.filetype
+  if filetype == "python" then
+    local command = "FloatermNew! --height=0.99 --width=0.99 python3 " .. current_file
+    vim.cmd(command)
+  elseif filetype == "lua" then
+    vim.cmd("luafile %")
+  end
+end
+
 -- Floating Terminal
-nremap("<leader>ex", require("jer.float_term").execute_code, "Execute Code in Floating Window")
+nremap("<leader>ex", execute_code, "Execute Code in Floating Window")
 nremap("<leader>vp", [[<CMD>VimuxPromptCommand<CR>]], "Run Command From Prompt in Tmux")
 nremap("<leader>vl", [[<CMD>VimuxRunLastCommand<CR>]], "Run Last command in Tmux")
 nremap("<leader>vv", [[<CMD>VimuxCloseRunner<CR>]], "Close Tmux window")
