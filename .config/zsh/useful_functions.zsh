8c0cd6a349a0c63574b5a6a70aacda2f466f43aa
Jeremy Mann <jmann277@gmail.com>
2022-04-15 11:00:51 -0700

    fix bugs with tmux functions

--- .config/zsh/useful_functions.zsh
+++ .config/zsh/useful_functions.zsh
@@ -1,6 +1,8 @@
 # fe [FUZZY PATTERN] - Open the selected file with the default editor
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
+#
+
 E() {
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
@@ -40,27 +42,51 @@ tr () {
     nvim . -c ":lua require('tarot').telescope_tarots()"
 }
 
+create_workstation() {
+    name=$1
+    directory=$2
+    tmux -f ${TMUX_CONFIG} new-session -d -s ${name} -c ${directory}
+    tmux list-sessions
+    tmux new-window -t ${name} -c ${directory}
+    tmux send-keys -t ${name}:0 "$EDITOR ." Enter
+}
+
+create_or_attach_work_session () {
+  name=$1
+  directory=$2
+  if [[ -n $TMUX ]]
+  then
+      if (tmux has-session -t ${name})
+      then
+          tmux -f ${TMUX_CONFIG} switch -t ${name}
+      else
+          create_workstation $name $directory
+          tmux -f ${TMUX_CONFIG} switch -t ${name}:0
+      fi
+  else
+      if (tmux has-session -t ${name})
+      then
+        tmux -f ${TMUX_CONFIG} attach -t ${name}
+      else
+        create_workstation $name $directory
+        tmux -f ${TMUX_CONFIG} attach -t ${name}:0
+      fi
+  fi
+}
+
+td () {
+  create_or_attach_work_session "pde" $DOTFILES
+}
+
 
 tp () {
   IFS=$'\n' files=($(ls $CODEDIR| fzf-tmux --multi --select-1 --exit-0))
   [[ -n "$files" ]] &&  folder="${files[@]}"
   directory=$CODEDIR/$folder
-  if [[ -n $TMUX ]]
-  then
-      if (tmux has-session -t ${folder})
-      then
-          tmux -f ${TMUX_CONFIG} switch -t ${folder}
-      else
-          tmux -f ${TMUX_CONFIG} new -s ${folder} -d -c ${directory}
-          tmux -f ${TMUX_CONFIG} switch -t ${folder}
-      fi
-  else
-      if (tmux has-session -t ${folder})
-      then
-        tmux -f ${TMUX_CONFIG} attach -t ${folder}   
-      else
-        tmux -f ${TMUX_CONFIG} new -s ${folder} -c ${directory}
-      fi
-  fi
+  create_or_attach_work_session $folder $directory
 }
 
+cf () {
+    project=($(ls $CODEDIR | fzf-tmux))
+    cd $CODEDIR/$project
+}
