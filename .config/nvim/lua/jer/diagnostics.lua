4c14b62dd421df997a899eb43584f0400908c325
Jeremy Mann <jmann277@gmail.com>
2022-05-08 09:51:13 -0700

    fix show diagnostics remap

--- .config/nvim/lua/jer/diagnostics.lua
+++ .config/nvim/lua/jer/diagnostics.lua
@@ -1,7 +1,7 @@
 local nremap = require("jer.keymaps").nremap
 local telescope = require "telescope.builtin"
 
-nremap("<leader>ds", vim.lsp.diagnostic.show_line_diagnostics, "Show Diagnostic")
+nremap("<leader>ds", vim.diagnostic.open_float, "Show Diagnostic")
 nremap("<leader>dj", vim.diagnostic.goto_next, "Go to Next Diagnostic")
 nremap("<leader>dk", vim.diagnostic.goto_prev, "Go to Previous Diagnostic")
 nremap("<leader>df", telescope.diagnostics, "Telescope Diagnostics")
