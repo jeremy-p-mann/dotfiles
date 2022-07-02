3226db6849b05115549857c768fea4c11c9884b4
Jeremy Mann <jmann227@gmail.com>
2022-07-02 08:10:47 -0700

    reorg plugins

--- .config/nvim/lua/jer/plugins.lua
+++ .config/nvim/lua/jer/plugins.lua
@@ -14,52 +14,66 @@ end
 vim.cmd [[packadd packer.nvim]]
 
 return require("packer").startup(function(use)
-  use "Pocco81/TrueZen.nvim"
-  use "hashivim/vim-terraform"
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+  use "nvim-lua/plenary.nvim"
+
   use "wbthomason/packer.nvim"
-  use "L3MON4D3/LuaSnip"
   use "tpope/vim-commentary"
+
   use "benmills/vimux"
   use "voldikss/vim-floaterm"
+  use "jpalardy/vim-slime"
+
   use "tpope/vim-capslock"
-  use "vim-test/vim-test"
-  use "lewis6991/gitsigns.nvim"
   use "nvim-lua/popup.nvim"
-  use "nvim-lua/plenary.nvim"
   use "/Users/jeremymann/Documents/code/tarot.nvim"
+
+  use "vim-test/vim-test"
+
+  use "lewis6991/gitsigns.nvim"
+
+  use "ThePrimeagen/harpoon"
   use "nvim-telescope/telescope.nvim"
   use "nvim-telescope/telescope-fzy-native.nvim"
+
+  use "Pocco81/TrueZen.nvim"
   use "norcalli/nvim-colorizer.lua"
   use "gruvbox-community/gruvbox"
   use "EdenEast/nightfox.nvim"
   use "rose-pine/neovim"
   use "aditya-azad/candle-grey"
+
   use "neovim/nvim-lspconfig"
 
+  use "tpope/vim-surround"
   use "nvim-treesitter/nvim-treesitter-textobjects"
+
   use "hrsh7th/nvim-cmp"
   use "hrsh7th/cmp-nvim-lsp"
-  use "tpope/vim-surround"
   use "hrsh7th/cmp-buffer"
   use "hrsh7th/cmp-path"
   use "hrsh7th/cmp-cmdline"
+  use "hrsh7th/cmp-nvim-lua"
+  use "onsails/lspkind-nvim"
+
+  use "L3MON4D3/LuaSnip"
   use "saadparwaiz1/cmp_luasnip"
   use "rafamadriz/friendly-snippets"
-  use "onsails/lspkind-nvim"
-  use "hrsh7th/cmp-nvim-lua"
+
   use "rcarriga/nvim-notify"
-  use "jpalardy/vim-slime"
+
   use "jose-elias-alvarez/null-ls.nvim"
-  use "rcarriga/nvim-dap-ui"
   use {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+  use "hashivim/vim-terraform"
+
+  use "rcarriga/nvim-dap-ui"
   use "mfussenegger/nvim-dap"
   use "mfussenegger/nvim-dap-python"
-  if packer_bootstrap then
-    require("packer").sync()
-  end
-  use "ThePrimeagen/harpoon"
+
   use "AckslD/nvim-neoclip.lua"
 end)
