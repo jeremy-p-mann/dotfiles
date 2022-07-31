13c688bd968139ff48abaeec929e05f13d816983
Jeremy Mann <jmann227@gmail.com>
2022-07-30 21:23:08 -0700

    ensure a bunch of treesitter parsers are installed

--- .config/nvim/lua/jer/syntax.lua
+++ .config/nvim/lua/jer/syntax.lua
@@ -1,6 +1,33 @@
 require("nvim-treesitter.configs").setup {
   highlight = { enable = true },
   indent = { enable = true },
+  ensure_installed = {
+    "c",
+    "html",
+    "typescript",
+    "python",
+    "rust",
+    "scala",
+    "lua",
+    "bash",
+    "sql",
+    "bash",
+    "astro",
+    "json",
+    "yaml",
+    "ruby",
+    "rst",
+    "markdown",
+    "latex",
+    "javascript",
+    "go",
+    "dockerfile",
+    "cpp",
+    "c_sharp",
+    "toml",
+    "vim",
+  },
+  auto_install = false,
 }
 require("nvim-treesitter.configs").setup {
   textobjects = {
@@ -19,24 +46,24 @@ require("nvim-treesitter.configs").setup {
       },
     },
   },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
-      },
+  move = {
+    enable = true,
+    set_jumps = true, -- whether to set jumps in the jumplist
+    goto_next_start = {
+      ["]m"] = "@function.outer",
+      ["]]"] = "@class.outer",
     },
+    goto_next_end = {
+      ["]M"] = "@function.outer",
+      ["]["] = "@class.outer",
+    },
+    goto_previous_start = {
+      ["[m"] = "@function.outer",
+      ["[["] = "@class.outer",
+    },
+    goto_previous_end = {
+      ["[M"] = "@function.outer",
+      ["[]"] = "@class.outer",
+    },
+  },
 }
