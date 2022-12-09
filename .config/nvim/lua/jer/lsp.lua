896e81b6cb3a62913a3688e29e567df616e33193
Jeremy Mann <jmann227@gmail.com>
2022-12-08 19:20:37 -0800

    fix random breaking change

--- .config/nvim/lua/jer/lsp.lua
+++ .config/nvim/lua/jer/lsp.lua
@@ -1,14 +1,49 @@
 --- LSP Related Stuff ---
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local on_attach = function()
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc="Hover Documentation"} )
-  vim.keymap.set("n", "gd", "<CMD>Telescope lsp_definitions<CR>", { buffer = 0, desc="Telescope Definitions"})
-  vim.keymap.set("n", "gi", "<CMD>Telescope lsp_implementations<CR>", { buffer = 0,  desc="Telescope Implementations"})
-  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0, desc="Go to Type Definition"})
-  vim.keymap.set("n", "<leader>rf", require("telescope.builtin").lsp_references, {desc="Telescope References"})
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = 0, desc="LSP Rename"})
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0, desc="LSP Code Actions"})
+  vim.keymap.set(
+    "n",
+    "K",
+    vim.lsp.buf.hover,
+    { buffer = 0, desc = "Hover Documentation" }
+  )
+  vim.keymap.set(
+    "n",
+    "gd",
+    "<CMD>Telescope lsp_definitions<CR>",
+    { buffer = 0, desc = "Telescope Definitions" }
+  )
+  vim.keymap.set(
+    "n",
+    "gi",
+    "<CMD>Telescope lsp_implementations<CR>",
+    { buffer = 0, desc = "Telescope Implementations" }
+  )
+  vim.keymap.set(
+    "n",
+    "gt",
+    vim.lsp.buf.type_definition,
+    { buffer = 0, desc = "Go to Type Definition" }
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>rf",
+    require("telescope.builtin").lsp_references,
+    { desc = "Telescope References" }
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>rn",
+    vim.lsp.buf.rename,
+    { buffer = 0, desc = "LSP Rename" }
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>ca",
+    vim.lsp.buf.code_action,
+    { buffer = 0, desc = "LSP Code Actions" }
+  )
 end
 
 local opts = {
@@ -24,44 +59,44 @@ require("null-ls").setup {
     -- null_ls.builtins.diagnostics.write_good,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.pg_format.with {
-      extra_args = {"-w", "80"},
+      extra_args = { "-w", "80" },
     },
   },
 }
 require("lspconfig").pylsp.setup(opts)
 
-require'lspconfig'.ansiblels.setup{
+require("lspconfig").ansiblels.setup {
   capabilities = capabilities,
   on_attach = on_attach,
 }
 
-require'lspconfig'.dockerls.setup(opts)
+require("lspconfig").dockerls.setup(opts)
 
-require'lspconfig'.texlab.setup(opts)
+require("lspconfig").texlab.setup(opts)
 
-require'lspconfig'.gopls.setup(opts)
+require("lspconfig").gopls.setup(opts)
 
-require'lspconfig'.terraformls.setup{
+require("lspconfig").terraformls.setup {
   capabilities = capabilities,
-    on_attach = on_attach,
-    filetypes={'tf'},
+  on_attach = on_attach,
+  filetypes = { "tf" },
 }
 
-require("lspconfig").tsserver.setup({
-    capabilities = opts.capabilities,
-    on_attach = function(client)
-        client.resolved_capabilities.document_formatting = false
-        on_attach()
-    end
-})
+require("lspconfig").tsserver.setup {
+  capabilities = opts.capabilities,
+  on_attach = function(client)
+    client.resolved_capabilities.document_formatting = false
+    on_attach()
+  end,
+}
 
 require("lspconfig").bashls.setup(opts)
 
 require("lspconfig").rust_analyzer.setup(opts)
 
-require'lspconfig'.html.setup{}
+require("lspconfig").html.setup {}
 
-require'lspconfig'.cssls.setup{}
+require("lspconfig").cssls.setup {}
 
 -- LUA
 -- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
@@ -78,7 +113,10 @@ end
 
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
 local sumneko_root_path = "/Users/jeremymann/Documents/code/lua-language-server"
-local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
+local sumneko_binary = sumneko_root_path
+  .. "/bin/"
+  .. system_name
+  .. "/lua-language-server"
 
 local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
