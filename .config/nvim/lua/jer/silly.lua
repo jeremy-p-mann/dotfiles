6611b5a0d9a34b3f8c7dfa8278d243c10033c375
Jeremy Mann <jmann227@gmail.com>
2023-12-29 09:06:17 -0800

    deprecate some silly stuff

--- .config/nvim/lua/jer/silly.lua
+++ .config/nvim/lua/jer/silly.lua
@@ -1,18 +1,12 @@
 local nremap = require("jer.keymaps").nremap
 
 nremap("<leader>tar", require("tarot").telescope_tarots, "Tarot")
-nremap("<leader>asc", [[<CMD> FloatermNew! --height=0.999 --width=0.999 asciiquarium<CR>]], "Float asciiquarium")
+
+-- update function
 local config = {
     fps = 35,
     name = 'snake',
 }
-
--- init function is invoked only once at the start
--- config.init = function (grid)
---
--- end
-
--- update function
 config.update = function (grid)
     for i = 1, #grid do
         local prev = grid[i][#(grid[i])]
@@ -22,26 +16,19 @@ config.update = function (grid)
     end
     return true
 end
-
-
--- require("cellular-automaton").register_animation(config)
-
--- require("zone").setup()
-
+require("cellular-automaton").register_animation(config)
 
 vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
 vim.keymap.set("n", "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>")
 vim.keymap.set("n", "<leader>sn", "<cmd>CellularAutomaton snake<CR>")
 
-            -- lkjasdlkfj
-nremap(
-  "<leader>asl",
-  [[<CMD>FloatermNew! --height=0.99 --width=0.99 asciiquarium \| lolcat<CR>]],
-        -- lkjasdlkfj                   lkadjf;lkjl;
-  "Float asciiquarium in lolcatz"
-)
-                    -- lkjasdlkfj                   lkadjf;lkjl;
+-- nremap(
+--   "<leader>asl",
+--   [[<CMD>FloatermNew! --height=0.99 --width=0.99 asciiquarium \| lolcat<CR>]],
+--   "Float asciiquarium in lolcatz"
+-- )
+-- nremap("<leader>asc", [[<CMD> FloatermNew! --height=0.999 --width=0.999 asciiquarium<CR>]], "Float asciiquarium")
+
 nremap("<leader>jo", require("jer.notify").get_joke, "Get A Joke")
 nremap("<leader>nn", [[<CMD>lua require('notify')('hi')<CR>]])
-        -- lkjasdlkfj                   lkadjf;lkjl;
 
