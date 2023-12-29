61a6a69032660c0a16c3b2a361c43ad6e5ec97c7
Jeremy Mann <jmann227@gmail.com>
2023-12-29 08:54:11 -0800

    refactor local module

--- .config/nvim/lua/jer/local.lua
+++ .config/nvim/lua/jer/local.lua
@@ -1,48 +1,45 @@
 local M = {}
 
-M.has__local = string.find(
-  vim.fn.system "ls $XDG_CONFIG_HOME/nvim/lua/jer",
-  "_local.lua"
-) ~= nil
+local function load_local_module()
+  local codedir = os.getenv "CODEDIR"
+  if not codedir then
+    vim.print('CODEDIR env not found!!!')
+    return false
+  end
+
+  local filepath = codedir .. "/local_nvim/jer_local.lua"
+  local module_name = "jer_local"
+  local file = io.open(filepath, "r")
+  if not file then
+    vim.print('local module not found!')
+    return false
+  end
+  file:close()
+
+  local module_dir = codedir .. "/local_nvim/?.lua"
+  if not string.find(package.path, module_dir, 1, true) then
+    package.path = package.path .. ";" .. module_dir
+  end
+
+  local status, module = pcall(require, module_name)
+  if not status then
+    vim.print('local module unable to be called!')
+    return false
+  end
+  return module
+end
 
 M.do_stuff = function()
-  local codedir = os.getenv "CODEDIR"
-  if codedir then
-    local filepath = codedir .. "/local_nvim/jer_local.lua"
-    local module_name = "jer_local"
-    local file = io.open(filepath, "r")
-    if file then
-      file:close()
-      -- Add the directory to package.path
-      local module_dir = codedir .. "/local_nvim/?.lua"
-      package.path = package.path .. ";" .. module_dir
-
-      local status, err = pcall(require, module_name)
-      if status then
-        require(module_name).do_stuff()
-      end
-    end
+  local module = load_local_module()
+  if module then
+    module.do_stuff()
   end
 end
 
 M.get_plugins = function()
-  local codedir = os.getenv "CODEDIR"
-  if codedir then
-    local filepath = codedir .. "/local_nvim/jer_local.lua"
-    local module_name = "jer_local"
-    local file = io.open(filepath, "r")
-    if file then
-      file:close()
-      -- Add the directory to package.path
-      local module_dir = codedir .. "/local_nvim/?.lua"
-      package.path = package.path .. ";" .. module_dir
-
-      local status, err = pcall(require, module_name)
-      if status then
-        local_plugins = require(module_name).plugins
-        return local_plugins
-      end
-    end
+  local module = load_local_module()
+  if module then
+    return module.plugins or {}
   end
   return {}
 end
