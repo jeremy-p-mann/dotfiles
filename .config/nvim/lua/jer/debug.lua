e118c6247f6270400f22598721ead874835a7596
Jeremy Mann <jmann227@gmail.com>
2024-03-22 18:05:16 -0700

    remove dap ui stuff

--- .config/nvim/lua/jer/debug.lua
+++ .config/nvim/lua/jer/debug.lua
@@ -1,27 +1,27 @@
 local nremap = require("jer.keymaps").nremap
 
-local dap = require('dap')
--- Python
-require('dap-python').test_runner = 'pytest'
-require('dap-python').setup('/usr/local/bin/python3')
-require("dapui").setup()
-dap.configurations.python = {
-  {
-    type = 'python';
-    request = 'launch';
-    name = "Launch file";
-    program = "${file}";
-    pythonPath = function()
-      local cwd = vim.fn.getcwd()
-      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-        return cwd .. '/venv/bin/python'
-      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-        return cwd .. '/.venv/bin/python'
-      else
-        return '/usr/bin/python'
-      end
-    end;
-  },
-}
+-- local dap = require('dap')
+-- -- Python
+-- require('dap-python').test_runner = 'pytest'
+-- require('dap-python').setup('/usr/local/bin/python3')
+-- require("dapui").setup()
+-- dap.configurations.python = {
+--   {
+--     type = 'python';
+--     request = 'launch';
+--     name = "Launch file";
+--     program = "${file}";
+--     pythonPath = function()
+--       local cwd = vim.fn.getcwd()
+--       if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+--         return cwd .. '/venv/bin/python'
+--       elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+--         return cwd .. '/.venv/bin/python'
+--       else
+--         return '/usr/bin/python'
+--       end
+--     end;
+--   },
+-- }
 
 nremap("<leader>bp", [[oimport pdb; pdb.set_trace()<C-c>]], "Add breakpoint")
