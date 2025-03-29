374438a0027a57c60012e4e57012fb29f94894c8
Jeremy Mann <jmann227@gmail.com>
2025-03-29 12:57:38 -0700

    discover python path better

--- .config/nvim/lua/jer/lsp.lua
+++ .config/nvim/lua/jer/lsp.lua
@@ -62,7 +62,34 @@ require("null-ls").setup {
   },
 }
 
-require("lspconfig").pyright.setup(opts)
+local configs = require "lspconfig/configs"
+local util = require "lspconfig/util"
+
+local path = util.path
+
+local function get_python_path(workspace)
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+        return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+    end
+
+    -- Find and use virtualenv in workspace directory.
+    for _, pattern in ipairs { "*", ".*" } do
+        local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+        if match ~= "" then
+            return path.join(vim.fs.dirname(match), "bin", "python")
+        end
+    end
+
+    return vim.fn.exepath "python3" or vim.fn.exepath "python" or "python"
+end
+
+local pyright_opts = opts
+pyright_opts.before_init = function(_, config)
+    config.settings.python.pythonPath = get_python_path(config.root_dir)
+end
+
+require("lspconfig").pyright.setup(pyright_opts)
 
 require("lspconfig").ansiblels.setup {
   capabilities = capabilities,
